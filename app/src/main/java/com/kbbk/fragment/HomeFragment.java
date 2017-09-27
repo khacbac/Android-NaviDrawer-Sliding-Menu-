495db575fd4bba355a88ae275c69ab51b0cae764
BacHK <hokhacbac@gmail.com>
2017-09-27 16:10:15 +0700

    Commit 27/09/2017 Test OK

--- app/src/main/java/com/kbbk/fragment/HomeFragment.java
+++ app/src/main/java/com/kbbk/fragment/HomeFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import info.androidhive.navigationdrawer.R;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,9 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    @BindView(R.id.imgIconHome)
+    ImageView imgIconHome;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -65,7 +72,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this,view);
+
+        imgIconHome.setImageDrawable(getResources().getDrawable(R.drawable.images_luffy));
+
+
+        // Allow zoom in,zoom out image view.
+        PhotoViewAttacher photoViewAttacher = new PhotoViewAttacher(imgIconHome);
+        photoViewAttacher.update();
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
